b64eaa33683c2054c2b7e4b7498d8f5e8b5cc43c
Mark Derricutt <mark@talios.com>
2014-09-23 23:53:05 +1200

    Added @Parameter stubs to generate required metadata
    This helps keep IDEs happy as the configuration elements
    are now expected.  Even if we don't use them.

--- src/main/groovy/io/repaint/maven/tiles/AbstractTileMojo.groovy
+++ src/main/groovy/io/repaint/maven/tiles/AbstractTileMojo.groovy
@@ -1,12 +1,9 @@
 package io.repaint.maven.tiles
-
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugins.annotations.Component
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.MavenProject
-import org.apache.maven.project.MavenProjectHelper
 import org.codehaus.plexus.logging.Logger
-
 /**
  *
  * @author: Richard Vowles - https://plus.google.com/+RichardVowles
@@ -16,6 +13,12 @@ abstract class AbstractTileMojo extends AbstractMojo {
 
 	@Parameter(property = "project", readonly = true, required = true)
 	MavenProject project
+
+    @Parameter(property = "tiles", readonly = false, required = false)
+    List<String> tiles
+
+    @Parameter(property = "buildSmells", readonly = false, required = false)
+    String buildSmells
 
 	@Component
 	Logger logger

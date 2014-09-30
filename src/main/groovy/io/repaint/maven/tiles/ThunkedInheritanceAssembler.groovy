a502f35ee8da6de8e410347f8a8b92baed01bd17
Richard Vowles <richard@bluetrainsoftware.com>
2014-09-30 23:01:25 +1300

    Fixed the weird and stupid code in the MavenModelMerger that is doing weird stuff to the SCM section.
    Change-Id: Ib9ed1a399029f932faec7d366fd9fd070923c471

---
+++ src/main/groovy/io/repaint/maven/tiles/ThunkedInheritanceAssembler.groovy
@@ -0,0 +1,51 @@
+package io.repaint.maven.tiles
+
+import org.apache.maven.model.Scm
+import org.apache.maven.model.inheritance.DefaultInheritanceAssembler
+
+/**
+ *
+ * @author: Richard Vowles - https://plus.google.com/+RichardVowles
+ */
+class ThunkedInheritanceAssembler extends DefaultInheritanceAssembler {
+	public static class FixedInheritanceAssembler extends DefaultInheritanceAssembler.InheritanceModelMerger {
+		@Override
+		protected void mergeScm_Url(Scm target, Scm source, boolean sourceDominant, Map<Object, Object> context) {
+			String src = source.getUrl();
+			if ( src != null )
+			{
+				if ( sourceDominant || target.getUrl() == null )
+				{
+					target.setUrl( src );
+					target.setLocation( "url", source.getLocation( "url" ) );
+				}
+			}
+		}
+
+		@Override
+		protected void mergeScm_Connection(Scm target, Scm source, boolean sourceDominant, Map<Object, Object> context) {
+			String src = source.getConnection();
+			if ( src != null )
+			{
+				if ( sourceDominant || target.getConnection() == null )
+				{
+					target.setConnection( src );
+					target.setLocation( "connection", source.getLocation( "connection" ) );
+				}
+			}
+		}
+
+		@Override
+		protected void mergeScm_DeveloperConnection(Scm target, Scm source, boolean sourceDominant, Map<Object, Object> context) {
+			String src = source.getDeveloperConnection();
+			if ( src != null )
+			{
+				if ( sourceDominant || target.getDeveloperConnection() == null )
+				{
+					target.setDeveloperConnection( src );
+					target.setLocation( "developerConnection", source.getLocation( "developerConnection" ) );
+				}
+			}
+		}
+	}
+}

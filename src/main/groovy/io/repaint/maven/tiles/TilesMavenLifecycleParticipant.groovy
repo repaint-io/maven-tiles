a55ad061291f7a25aa6b36158e7b8a0f960ae279
Mark Derricutt <mark@talios.com>
2014-10-02 21:36:02 +1300

    Improved logging of tile injection.
    Change-Id: If098cc43ed22484b56d9c4566388ea78d37abff0

--- src/main/groovy/io/repaint/maven/tiles/TilesMavenLifecycleParticipant.groovy
+++ src/main/groovy/io/repaint/maven/tiles/TilesMavenLifecycleParticipant.groovy
@@ -398,7 +398,10 @@ public class TilesMavenLifecycleParticipant extends AbstractMavenLifecyclePartic
 		Model lastPom = pomModel
 		File lastPomFile = request.pomFile
 
-		StringBuilder sb = new StringBuilder()
+		if (tiles) {
+			logger.info("--- tiles-maven-plugin: Injecting ${tiles.size()} tiles as intermediary parent artifact's...")
+			logger.info("Mixed '${modelGav(pomModel)}' with tile '${modelGav(tiles.first().model)}' as it's new parent.")
+		}
 
 		tiles.each { TileModel tileModel ->
 			Model model = tileModel.model
@@ -406,10 +409,9 @@ public class TilesMavenLifecycleParticipant extends AbstractMavenLifecyclePartic
 			Parent modelParent = new Parent(groupId: model.groupId, version: model.version, artifactId: model.artifactId)
 			lastPom.parent = modelParent
 
-			sb.append("model: '${modelGav(lastPom)}' has tile '${parentGav(modelParent)}' as new parent.\n")
-
 			if (pomModel != lastPom) {
 				putModelInCache(lastPom, request, lastPomFile)
+				logger.info("Mixed '${modelGav(lastPom)}' with tile '${parentGav(modelParent)}' as it's new parent.")
 			}
 
 			lastPom = model
@@ -417,13 +419,13 @@ public class TilesMavenLifecycleParticipant extends AbstractMavenLifecyclePartic
 		}
 
 		lastPom.parent = originalParent
-		sb.append("model: '${modelGav(lastPom)}' has original pom '${parentGav(originalParent)}' as new parent.")
+		logger.info("Mixed '${modelGav(lastPom)}' with original parent '${parentGav(originalParent)}' as it's  new top level parent.")
+		logger.info("")
 
 		if (pomModel != lastPom) {
 			putModelInCache(lastPom, request, lastPomFile)
 		}
 
-		logger.info(sb.toString())
 	}
 
 	protected void copyModel(Model projectModel, Model newModel) {

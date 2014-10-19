dd6ea3a0b97276fb5b64aba88a9358dbe1ceebdc
Mark Derricutt <mark@talios.com>
2014-10-19 22:39:24 +1300

    Added support for resource filtering
    A tile.xml file can now include such things as @project.version@
    and be replaced with the current project values if
    <filtering>true</filtering> is configured. This defaults to false.
    
    This will ONLY replace @var@ references inside the tile.xml.
    
    Change-Id: If3bfc6a10c3299b76e3e78299fe5f8faf191cd88

--- src/main/groovy/io/repaint/maven/tiles/AbstractTileMojo.groovy
+++ src/main/groovy/io/repaint/maven/tiles/AbstractTileMojo.groovy
@@ -1,12 +1,17 @@
 package io.repaint.maven.tiles
-
 import groovy.transform.CompileStatic
+import org.apache.maven.execution.MavenSession
+import org.apache.maven.model.Resource
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugins.annotations.Component
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.MavenProject
+import org.apache.maven.shared.filtering.MavenFileFilter
+import org.apache.maven.shared.filtering.MavenFileFilterRequest
+import org.apache.maven.shared.filtering.MavenResourcesExecution
+import org.apache.maven.shared.filtering.MavenResourcesFiltering
+import org.apache.maven.shared.utils.io.FileUtils
 import org.codehaus.plexus.logging.Logger
-
 /**
  *
  * @author: Richard Vowles - https://plus.google.com/+RichardVowles
@@ -25,10 +30,51 @@ abstract class AbstractTileMojo extends AbstractMojo {
 	@Parameter(property = "buildSmells", readonly = false, required = false)
 	String buildSmells
 
+	@Parameter(property = "filtering", readonly = false, required = false, defaultValue = "false")
+	boolean filtering
+
+	@Parameter(required = true, defaultValue = "\${project.build.directory}/generated-sources")
+	protected File generatedSourcesDirectory;
+
 	@Component
 	Logger logger
 
+	@Component
+	MavenSession mavenSession
+
+	@Component
+	MavenFileFilter mavenFileFilter
+
+	@Component
+	MavenResourcesFiltering mavenResourcesFiltering
+
 	File getTile() {
-		return new File(project.getBasedir(), TILE_POM)
+		File baseTile = new File(project.basedir, TILE_POM)
+		if (filtering) {
+			File processedTileDirectory = new File(generatedSourcesDirectory, "tiles")
+			processedTileDirectory.mkdirs()
+			File processedTile = new File(processedTileDirectory, TILE_POM);
+
+
+			Resource tileResource = new Resource()
+			tileResource.setDirectory(project.basedir.absolutePath);
+			tileResource.includes.add(TILE_POM)
+			tileResource.setFiltering(true)
+
+			MavenFileFilterRequest req = new MavenFileFilterRequest(baseTile, processedTile, true, project, [], true, "UTF-8", mavenSession, new Properties())
+			req.setDelimiters(new LinkedHashSet(["@"]))
+
+			List<FileUtils.FilterWrapper> filterWappers = mavenFileFilter.getDefaultFilterWrappers(req)
+
+			MavenResourcesExecution execution = new MavenResourcesExecution(
+					[tileResource], processedTileDirectory, "UTF-8", filterWappers, project.basedir,
+					mavenResourcesFiltering.defaultNonFilteredFileExtensions)
+
+			mavenResourcesFiltering.filterResources(execution);
+
+			return new File(processedTileDirectory, TILE_POM);
+		} else {
+			return new File(project.basedir, TILE_POM);
+		}
 	}
 }

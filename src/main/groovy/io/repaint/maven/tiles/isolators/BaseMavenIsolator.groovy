9a02b9780ad2df63274a262e3a284d7432521308
Pierre-Louis JAEGER <pilou@tour-bois.com>
2015-11-24 02:23:01 +0100

    Add ability to use version ranges for plugins declared in tiles
    This feature reduce the risk of bad plugin version resolution when the
    same plugin is used in multiple tiles. With "rigid" plugin version, the
    plugin could resolve to an old version which for example don't include
    required goals.
    
    Also fail the build if intersection of plugin version ranges from
    different tiles is overconstrainted and results in an empty set of
    acceptable versions.
    
    Change-Id: I8f39ecd5426842c1803c8d7bfbe6709ad4f5dd99

--- src/main/groovy/io/repaint/maven/tiles/isolators/BaseMavenIsolator.groovy
+++ src/main/groovy/io/repaint/maven/tiles/isolators/BaseMavenIsolator.groovy
@@ -3,9 +3,14 @@ package io.repaint.maven.tiles.isolators
 import org.apache.maven.MavenExecutionException
 import org.apache.maven.RepositoryUtils
 import org.apache.maven.artifact.Artifact
+import org.apache.maven.artifact.DefaultArtifact
 import org.apache.maven.artifact.repository.ArtifactRepository
+import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.execution.MavenSession
+import org.apache.maven.model.Plugin
 import org.apache.maven.model.building.ModelBuildingRequest
+import org.apache.maven.project.artifact.PluginArtifact
+import org.apache.maven.project.artifact.PluginArtifact.PluginArtifactHandler
 
 /**
  *
@@ -17,6 +22,7 @@ abstract class BaseMavenIsolator implements MavenVersionIsolator {
 	def versionRangeResolver
 	def repositorySystemSession
 	List<ArtifactRepository> remoteRepositories
+	List<ArtifactRepository> pluginRepositories
 	ArtifactRepository localRepository
 
 	/**
@@ -27,6 +33,7 @@ abstract class BaseMavenIsolator implements MavenVersionIsolator {
 	public BaseMavenIsolator(MavenSession mavenSession) throws MavenExecutionException {
 		this.repositorySystemSession = mavenSession.repositorySession
 		this.remoteRepositories = mavenSession.request.remoteRepositories
+		this.pluginRepositories = mavenSession.request.pluginArtifactRepositories
 		this.localRepository = mavenSession.request.localRepository
 
 		try {
@@ -58,6 +65,32 @@ abstract class BaseMavenIsolator implements MavenVersionIsolator {
 				versionRangeResult.versions.each { Object version ->
 					if(!version.toString().endsWith("-SNAPSHOT")) {
 						tileArtifact.version = version
+					}
+				}
+			}
+		}
+	}
+
+	public void resolvePluginVersionRange(Plugin plugin, VersionRange versionRange, boolean allowSnapshots) {
+		plugin.version = versionRange.toString()
+		def pluginArtifact = new PluginArtifact(plugin,new DefaultArtifact(
+			plugin.groupId, plugin.artifactId, versionRange, "compile","maven-plugin", null, new PluginArtifactHandler()))
+
+		def versionRangeRequest = versionRangeRequestClass.newInstance(RepositoryUtils.toArtifact(pluginArtifact),
+			RepositoryUtils.toRepos(pluginRepositories), null)
+
+		def versionRangeResult = versionRangeResolver.resolveVersionRange(repositorySystemSession, versionRangeRequest)
+
+		// Allow SNAPSHOT only if one of the bounds of version range is SNAPSHOT
+		boolean doAllowSnaphsots = allowSnapshots || versionRange.toString().contains("-SNAPSHOT")
+
+		if (versionRangeResult.versions) {
+			if(doAllowSnaphsots) {
+				plugin.version = versionRangeResult.highestVersion
+			} else {
+				versionRangeResult.versions.each { Object version ->
+					if(!version.toString().endsWith("-SNAPSHOT")) {
+						plugin.version = version
 					}
 				}
 			}

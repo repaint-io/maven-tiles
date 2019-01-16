55593a4057c9c9d1e1e62be36e2f54d1db2096cb
Brian Vella <brian.vella@ixaris.com>
2019-01-16 09:59:46 +0100

    added missing constants file

---
+++ src/main/groovy/io/repaint/maven/tiles/Constants.groovy
@@ -0,0 +1,11 @@
+package io.repaint.maven.tiles;
+
+public final class Constants {
+
+    public static final TILEPLUGIN_GROUP = 'io.repaint.maven'
+    public static final TILEPLUGIN_ARTIFACT = 'tiles-maven-plugin'
+    public static final TILE_POM = 'tile.xml'
+
+    private Constants() {}
+
+}

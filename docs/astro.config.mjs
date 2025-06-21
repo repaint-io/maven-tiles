ac751a4e233c3518c6b06e146afe0cad1e887e95
Mark Derricutt <mark.derricutt@smxemail.com>
2025-06-21 21:51:45 +1200

    Updated astra/starlight
    Change-Id: I6a6a6964c11655ddace0e37d8292c158cd91ee57

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,40 +1,45 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig, passthroughImageService } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://repaint-io.github.io',
-  base: '/maven-tiles',
-	integrations: [
-		starlight({
-			title: 'Tiles - Mixins For Apache Maven',
-			customCss: [
-				'./src/styles/custom.css',
-			],
-			social: {
-				github: 'https://github.com/repaint-io/maven-tiles',
-			},
-			sidebar: [
-				{
-					label: 'Maven Tiles',
-					items: [
-						{ label: "About", link: '/introduction/about/'},
-						{ label: "Using", link: '/introduction/using/'},
-						{ label: "Writing", link: '/introduction/writing/'}
-					]
-				},
-				// {
-				//   	label: 'Guides',
-				//   	items: [
-				//   		// Each item here is one entry in the navigation menu.
-				//   		{ label: 'Example Guide', link: '/guides/example/' }
-				//   	],
-				//   }
-				// {
-				// 	label: 'Reference',
-				// 	autogenerate: { directory: 'reference' },
-				// },
-			],
-		}),
-	],
+  site: "https://repaint-io.github.io",
+  base: "/maven-tiles",
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [
+    starlight({
+      title: "Tiles - Mixins For Apache Maven",
+      customCss: ["./src/styles/custom.css"],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/repaint-io/maven-tiles",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Maven Tiles",
+          items: [
+            { label: "About", link: "/introduction/about/" },
+            { label: "Using", link: "/introduction/using/" },
+            { label: "Writing", link: "/introduction/writing/" },
+          ],
+        },
+        // {
+        //   	label: 'Guides',
+        //   	items: [
+        //   		// Each item here is one entry in the navigation menu.
+        //   		{ label: 'Example Guide', link: '/guides/example/' }
+        //   	],
+        //   }
+        // {
+        // 	label: 'Reference',
+        // 	autogenerate: { directory: 'reference' },
+        // },
+      ],
+    }),
+  ],
 });
